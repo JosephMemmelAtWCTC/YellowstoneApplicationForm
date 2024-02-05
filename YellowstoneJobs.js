ee066d0845b010799ea9365f4347735921e3d40c
42null <averyparanoiduser@gmail.com>
2024-02-04 22:51:58 -0600

    Move to next page & personal infromation section

--- YellowstoneJobs.js
+++ YellowstoneJobs.js
@@ -20,18 +20,28 @@ $(function() {
     // .detach().appendTo($insideFormActiveHoldingArea);
     // $insideFormActiveHoldingArea.append($formPage1);
 
-
+    let currentPage = null;
 
     class ApplicationPage{
-        constructor(showName, jquerySelector, $submitArea){
+
+        constructor(showName, jquerySelector, $submitArea, startingPercentageInt){
             this.showName = showName;
             this.$jqueryPageElement = $(jquerySelector);
             this.headerTitleText = $("#formPage2").data("section");
             this.$submitArea = $submitArea;
+            this.startingPercentageInt = startingPercentageInt;
+
+            // Progress bar
+            this.$applicationProgressBar = $("#applicationProgressBar");
+        }
+
+        setPageProgressBar(percentage){
+            this.$applicationProgressBar.width(percentage+"%");
+            this.$applicationProgressBar.attr("aria-valuenow", percentage);
         }
 
         loadIntoApplication(){
-            console.log("formPage");
+            currentPage = this;
 
             //Ensure only page
             // console.log("PagesCount +"+$allFormPages.length)
@@ -47,10 +57,14 @@ $(function() {
             $applicationFooter.append(this.$submitArea);
 
             $pageTitleText.text(this.headerTitleText);
+            this.setPageProgressBar(this.startingPercentageInt);
         }
 
         moveToNext(){
             this.next.loadIntoApplication();
+        }
+        moveToPrevious(){
+            this.previous.loadIntoApplication();
         }
     }
 
@@ -59,16 +73,18 @@ $(function() {
     let displayIndex = 1;
     // TODO: Turn into a class
     const pages = [
-        new ApplicationPage(displayIndex++, "#formPage1", $continueOnlyIfAllAcceptedSubmitSection),
-        new ApplicationPage(displayIndex++, "#formPage2", $paginationAfter),
+        new ApplicationPage(displayIndex++, "#formPage1", $continueOnlyIfAllAcceptedSubmitSection, 0),
+        new ApplicationPage(displayIndex++, "#formPage2", $paginationAfter, 20),
     ];
-
     pages[0].loadIntoApplication();
-    // pages[1].loadIntoApplication();
+
 
     // Setup pagination
-    for(let i = 0; i < pages; i++) {
-
+    for(let i = 0; i < pages.length-1; i++) {
+        pages[i].next = pages[i+1];
+    }
+    for(let i = 1; i < pages.length; i++) {
+        pages[i].previous = pages[i-1];
     }
 
 
@@ -101,41 +117,48 @@ $(function() {
 
     }
 
-});
 
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-    'use strict'
+    (() => {
+        'use strict'
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
 
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            let completelyValidated = true;
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                let completelyValidated = true;
 
-            event.preventDefault()
-            if (!form.checkValidity()) {
-                event.stopPropagation()
-                completelyValidated = false;
-            }
-            form.classList.add('was-validated')
-
-            if(completelyValidated){
-                form.classList.add("completelyValidated");
-                if(form.id === "applicationForm"){
-                    console.log("Form application");
-                }else{
-                    console.log("NOT +"+form.id);
+                event.preventDefault()
+                if (!form.checkValidity()) {
+                    event.stopPropagation()
+                    completelyValidated = false;
                 }
-            }else{
-                console.log("NOT");
-            }
-        }, false)
-    })
-})()
+                form.classList.add('was-validated')
+
+                // console.log(event.submitter);
+                const submitterId = event.submitter.id;
+
+                if(submitterId === "pageMoveToPrevious"){
+                    currentPage.moveToPrevious();
+                }
+                if(completelyValidated){
+                    form.classList.add("completelyValidated");
+                    console.log(`Page "#${form.id}" validated`);
+
+                    if(submitterId === "startApplicationButton" || submitterId === "pageMoveToNext"){
+                        currentPage.moveToNext();
+                    }else{
+                        currentPage.moveToNext();
+                    }
+                }else{
+                    console.log(`Page "#${form.id}" unable to  be validatidated`);
+                }
+            }, false)
+        })
+    })()
+});
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
